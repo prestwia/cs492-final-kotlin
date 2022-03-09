6d41890a15de93670db81b43aeec226f159dc99b
JoshMinyard <71729031+JoshMinyard@users.noreply.github.com>
2022-03-08 17:11:37 -0800

    updated gradle and added intent to define a word (#6)
    Co-authored-by: Josh Minyard <joshdaminyard@gmail.com>

--- app/src/main/java/com/example/hangman/EndScreenActivity.kt
+++ app/src/main/java/com/example/hangman/EndScreenActivity.kt
@@ -1,12 +1,19 @@
 package com.example.hangman
 
+import android.app.SearchManager
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
 class EndScreenActivity : AppCompatActivity() {
+
+    //change this to the word from the api
+    val apiWord = "taco"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_screen)
@@ -18,7 +25,19 @@ class EndScreenActivity : AppCompatActivity() {
             Toast.makeText(this@EndScreenActivity, R.string.btn_play_again, Toast.LENGTH_SHORT).show()
         }
         button_define?.setOnClickListener(){
-            Toast.makeText(this@EndScreenActivity, R.string.btn_define, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this@EndScreenActivity, R.string.btn_define, Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
+                putExtra(SearchManager.QUERY, getString(
+                    R.string.definition,
+                    apiWord
+                ))
+            }
+            try{
+                startActivity(intent)
+            }catch(e: ActivityNotFoundException) {
+                Toast.makeText(this@EndScreenActivity, getString(R.string.search_error), Toast.LENGTH_SHORT).show()
+            }
+
         }
         button_menu?.setOnClickListener(){
             val intent = Intent(this, MainActivity::class.java)
